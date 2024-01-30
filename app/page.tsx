7c61f884bbb8553ac0993569fe2fa01bc5e263ff
cenk ergen <cenk.ergen@ext.kocsistem.com.tr>
2024-01-30 20:25:52 +0300

    make scrollindicator functional

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
           <ScrollIndicator />
         </div>
       </section>
-      <section className="flex flex-col justify-between items-center min-h-screen md:min-h-fit bg-[#f1f1f1] py-24">
+      <section className="flex flex-col justify-between items-center min-h-screen md:min-h-fit bg-[#f1f1f1] py-24" id="cards-section">
         <div className="container mx-auto">
           <h4 className="lg:text-5xl text-2xl font-bold text-center mb-12 text-b max-w-[726px] mx-auto lg:leading-relaxed leading-relaxed">KESiN’le çekilişlere katılmak hem KOLAY hem de ÜCRETSİZ</h4>
           <ul className="gap-5 flex flex-col lg:flex-row mx-2 justify-center place-items-center items-stretch">
--- components/scroll-indicator.tsx
+++ components/scroll-indicator.tsx
@@ -1,6 +1,8 @@
+'use client'
+
 export default function ScrollIndicator() {
     return (
-        <div className="bg-[#fafafa] rounded-full text-black animate-bounce p-2">
+        <div className="bg-[#fafafa] rounded-full text-black animate-bounce p-2" onClick={() => document.getElementById('cards-section')?.scrollIntoView({behavior: 'smooth'})}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
             </svg>
